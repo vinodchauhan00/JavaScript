c910eaaa7d783c78ac40b869d36071aa6d3746c2
Vinod Chauhan <136825472+vinodchauhan00@users.noreply.github.com>
2025-04-19 21:32:59 +0530

    Update constants04.js

--- constants04.js
+++ constants04.js
@@ -1,5 +1,5 @@
 // constants  declare
 
 const pi=3.14;
-// pi=3.15  this is error constants values not change .......
+// pi=3.15  this is error constants values not change ......
 console.log(pi);
