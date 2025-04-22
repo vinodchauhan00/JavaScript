e6a4533c887c25d35813644f8ac953200a44bb57
vinod <vinod890@gmail.com>
2025-04-22 10:45:30 +0530

    2204

---
+++ Datatypes.js
@@ -0,0 +1,34 @@
+//types of operator
+
+//data types(Primittive data types)
+//string "vinod"
+//number 33,4.5
+//booleans
+//undefind
+//null
+//Bigint
+//symbol
+
+
+let age =21;
+let firstname="vinod";
+
+console.log(typeof firstname);
+
+
+//convert number to string
+
+console.log(typeof(age +""));
+
+
+
+//convert string to number 
+
+let str =str +"vini";
+console.log(typeof str);
+
+//second method  convert in number to string ...
+
+let Age=18;
+Age=string(Age);
+console.log( typeof Age +"Age");
